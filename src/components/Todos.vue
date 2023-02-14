aa9400ac2c4f68c234d830a5bd07e41008d9519f
olivierraude <olivierraude@gmail.com>
2023-02-13 21:28:53 -0500

    ajout features gestion de tâches, tri des tâches

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,69 @@
 <template>
-    <section class="todoapp">
-        <header class="header">
-            <h1>Todos</h1>
-        </header>
-    </section>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todos</h1>
+      <input
+        type="text"
+        class="new-todo"
+        placeholder="Ajouter une tache"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+      />
+    </header>
+    <div class="main">
+      <ul class="todo-list">
+        <li
+          class="todo"
+          v-for="todo in todos"
+          :class="{ completed: todo.completed }"
+        >
+          <div class="view">
+            <input class="toggle" type="checkbox" v-model="todo.completed" />
+            <label>{{ todo.name }}</label>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <footer class="footer">
+      <span class="todo-count">
+        <strong>{{ remaining }}</strong> tâches à faire
+      </span>
+      <ul class="filters">
+        <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
+        <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">À faire</a></li>
+        <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
+      </ul>
+    </footer>
+  </section>
 </template>
 
 <script>
-    export default {
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          name: "tache test",
+          completed: false,
+        },
+      ],
+      newTodo: "",
+      filter: "all",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        completed: false,
+        name: this.newTodo,
+      });
+      this.newTodo = "";
+    },
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter(todo => !todo.completed).length
     }
+  }
+};
 </script>
